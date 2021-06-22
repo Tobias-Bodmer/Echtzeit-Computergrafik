1c134cdd06f8bdccf0ad8801b8cc3096807b9cfb
Tobias Bodmer <48796225+Tobias-Bodmer@users.noreply.github.com>
2021-06-22 18:16:03 +0200

    Update RTCG.js

--- Abschluss/src/rtcg-app/RTCG.js
+++ Abschluss/src/rtcg-app/RTCG.js
@@ -167,7 +167,6 @@ class RTCG {
         console.log(intersects);
 
         if (intersects[0] != undefined && enemies.find(enemy => enemy.geometry == intersects[0].object) == null) {
-            alert("click!");
             player.geometry.position.x = Math.round(intersects[0].point.x);
             player.geometry.position.z = Math.round(intersects[0].point.z);
         } else if (intersects[0] != undefined && enemies.find(enemy => enemy.geometry == intersects[0].object) != null) {
