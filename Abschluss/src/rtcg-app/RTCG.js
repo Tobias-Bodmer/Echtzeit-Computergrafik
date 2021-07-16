c0d1fb26f8291491ee846c693c4694421397f874
Tobias Bodmer <48796225+Tobias-Bodmer@users.noreply.github.com>
2021-07-16 20:00:18 +0200

    Update RTCG.js

--- Abschluss/src/rtcg-app/RTCG.js
+++ Abschluss/src/rtcg-app/RTCG.js
@@ -21,6 +21,10 @@ let currentEnemy;
 let stateScreen = document.getElementById("states");;
 let enemies = [];
 
+let gameBoard;
+let positionAttribute;
+
+
 let haveDiced = false;
 
 let anim_loop;
@@ -58,21 +62,22 @@ class RTCG {
 
     async iniLevel() {
         let group = new THREEM.Group();
-        const gameBoard = createGameBoard(21, 21, "white", "./src/texture/new_DefaultMaterial_BaseColor.png", "./src/texture/new_DefaultMaterial_Normal.png", true);
+        gameBoard = createGameBoard(21, 21, "white", "./src/texture/new_DefaultMaterial_BaseColor.png", "./src/texture/new_DefaultMaterial_Normal.png", true);
         gameBoard.forEach(element => {
             group.add(element);
+            positionAttribute = element.geometry.getAttribute('position');
         });
 
         const enemy = new Character("1", 100, 30, 20, 40, "./src/img/Sixfeet_colorFinal.jpg");
         await enemy.loadModel("./src/modells/Dwarf.gltf");
-        enemy.geometry.position.y = 1;
+        enemy.geometry.position.y = 5;
         enemy.geometry.position.z = -18;
         enemies.push(enemy);
         group.add(enemy.geometry);
 
         player = new Character("Player", 100, 70, 30, 20, "./src/img/dwarf_color1.jpg");
         await player.loadModel("./src/modells/Dwarf.gltf");
-        player.geometry.position.y = 1;
+        player.geometry.position.y = 5;
         player.geometry.rotation.y = Math.PI;
         player.geometry.position.z = -1;
         group.add(player.geometry);
@@ -82,11 +87,21 @@ class RTCG {
         scene.add(light.ambientLight);
 
         group.applyMatrix4(new THREE.Matrix4().makeScale(0.02, 0.02, 0.02));
-
-        //TODO: play with the numebr
         group.applyMatrix4(new THREE.Matrix4().makeTranslation(0, -0.2, 0));
 
         scene.add(group);
+
+        var target = new THREE.Vector3(); // create once an reuse it
+
+        var ray = new THREEM.Raycaster();
+
+        enemy.geometry.getWorldPosition(target);
+        ray.set(target, new THREE.Vector3(0, -0.02, 0));
+        enemy.geometry.position.y -= ray.intersectObjects(scene.children[3].children, false)[0].distance + 3;
+
+        player.geometry.getWorldPosition(target);
+        ray.set(target, new THREE.Vector3(0, -0.02, 0));
+        player.geometry.position.y -= ray.intersectObjects(scene.children[3].children, false)[0].distance + 3;
     }
 
     iniStates() {
@@ -135,11 +150,22 @@ class RTCG {
 
         if (intersects[0] != undefined && enemies.find(enemy => enemy.geometry == intersects[0].object) == null) {
             player.geometry.position.x = Math.round(intersects[0].point.x / 0.02);
+            player.geometry.position.y = 5;
             player.geometry.position.z = Math.round(intersects[0].point.z / 0.02);
+
+            var playerRay = new THREEM.Raycaster();
+            playerRay.set(new THREE.Vector3(intersects[0].point.x, 5, intersects[0].point.z), new THREE.Vector3(0, -1, 0));
+            player.geometry.position.y -= playerRay.intersectObjects(scene.children[3].children, false)[0].distance - 2;
+
         } else if (intersects[0] != undefined && enemies.find(enemy => enemy.geometry == intersects[0].object) != null) {
             if (player.geometry.position.distanceTo(intersects[0].object.position) > 1.5) {
                 player.geometry.position.x = intersects[0].object.position.x;
+                player.geometry.position.y = 5;
                 player.geometry.position.z = intersects[0].object.position.z + 1;
+
+                var playerRay = new THREEM.Raycaster();
+                playerRay.set(new THREE.Vector3(intersects[0].point.x, 5, intersects[0].point.z), new THREE.Vector3(0, -1, 0));
+                player.geometry.position.y -= playerRay.intersectObjects(scene.children[3].children, false)[0].distance - 2;
             }
 
             setTimeout(() => {
